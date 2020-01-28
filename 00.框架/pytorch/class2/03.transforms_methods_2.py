fab441a08e197810b992423bbdb6342d5cd51c99
sadicLiu <190978752@qq.com>
2020-01-28 14:32:16 +0800

    03 transforms done

---
+++ 00.框架/pytorch/class2/03.transforms_methods_2.py
@@ -0,0 +1,122 @@
+import os
+import random
+
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+import PIL.Image as Image
+
+from my_dataset import RMBDataset
+
+
+def set_seed(seed=1):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+
+set_seed(1)  # 设置随机种子
+
+# 参数设置
+BATCH_SIZE = 1
+LR = 0.01
+log_interval = 10
+val_interval = 1
+rmb_label = {"1": 0, "100": 1}
+
+
+def transform_invert(img_, transform_train):
+    """
+    将data 进行反transfrom操作
+    :param img_: tensor
+    :param transform_train: torchvision.transforms
+    :return: PIL image
+    """
+    if 'Normalize' in str(transform_train):
+        norm_transform = list(filter(lambda x: isinstance(x, transforms.Normalize), transform_train.transforms))
+        mean = torch.tensor(norm_transform[0].mean, dtype=img_.dtype, device=img_.device)
+        std = torch.tensor(norm_transform[0].std, dtype=img_.dtype, device=img_.device)
+        img_.mul_(std[:, None, None]).add_(mean[:, None, None])
+
+    img_ = img_.transpose(0, 2).transpose(0, 1)  # C*H*W --> H*W*C
+    img_ = np.array(img_) * 255
+
+    if img_.shape[2] == 3:
+        img_ = Image.fromarray(img_.astype('uint8')).convert('RGB')
+    elif img_.shape[2] == 1:
+        img_ = Image.fromarray(img_.astype('uint8').squeeze())
+    else:
+        raise Exception("Invalid img shape, expected 1 or 3 in axis 2, but got {}!".format(img_.shape[2]))
+
+    return img_
+
+
+# ============================ step 1/5 数据 ============================
+train_dir = "/home/liuhy/res/deep-learning/00.框架/pytorch/class2/01.rmb/dataset/rmb_split/train"
+
+norm_mean = [0.485, 0.456, 0.406]
+norm_std = [0.229, 0.224, 0.225]
+
+train_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+
+    # 1 Pad
+    # transforms.Pad(padding=32, fill=(255, 0, 0), padding_mode='constant'),
+    # transforms.Pad(padding=(8, 64), fill=(255, 0, 0), padding_mode='constant'),
+    # transforms.Pad(padding=(8, 16, 32, 64), fill=(255, 0, 0), padding_mode='constant'),
+    # transforms.Pad(padding=(8, 16, 32, 64), fill=(255, 0, 0), padding_mode='symmetric'),
+
+    # 2 ColorJitter
+    # transforms.ColorJitter(brightness=0.5),
+    # transforms.ColorJitter(contrast=0.5),
+    # transforms.ColorJitter(saturation=0.5),
+    # transforms.ColorJitter(hue=0.3),
+
+    # 3 Grayscale
+    # transforms.Grayscale(num_output_channels=3),
+
+    # 4 Affine
+    # transforms.RandomAffine(degrees=30),
+    # transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), fillcolor=(255, 0, 0)),
+    # transforms.RandomAffine(degrees=0, scale=(0.7, 0.7)),
+    # transforms.RandomAffine(degrees=0, shear=(0, 0, 0, 45)),
+    # transforms.RandomAffine(degrees=0, shear=90, fillcolor=(255, 0, 0)),
+
+    # 5 Erasing
+    # transforms.ToTensor(),
+    # transforms.RandomErasing(p=1, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=(254/255, 0, 0)),
+    # transforms.RandomErasing(p=.3, scale=(0.02, 0.33), ratio=(0.3, 3.3), value='1234'),
+
+    # 1 RandomChoice
+    # transforms.RandomChoice([transforms.RandomVerticalFlip(p=1), transforms.RandomHorizontalFlip(p=1)]),
+
+    # 2 RandomApply
+    # transforms.RandomApply([transforms.RandomAffine(degrees=0, shear=45, fillcolor=(255, 0, 0)),
+    #                         transforms.Grayscale(num_output_channels=3)], p=0.5),
+    # 3 RandomOrder
+    transforms.RandomOrder([transforms.RandomRotation(15),
+                            transforms.Pad(padding=32),
+                            transforms.RandomAffine(degrees=0, translate=(0.01, 0.1), scale=(0.9, 1.1))]),
+
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std),
+])
+
+train_data = RMBDataset(data_dir=train_dir, transform=train_transform)
+train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+# ============================ step 5/5 训练 ============================
+for i, data in enumerate(train_loader):
+    inputs, labels = data  # B C H W
+
+    img_tensor = inputs[0, ...]  # C H W
+    img = transform_invert(img_tensor, train_transform)
+    plt.imshow(img)
+    plt.show()
+    plt.pause(0.5)
+    plt.close()
+
+    if i > 5: break
