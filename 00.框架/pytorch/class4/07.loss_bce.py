57f093099e9fbfa617b12107d3a279bbc7a67fc5
sadicLiu <190978752@qq.com>
2020-01-30 15:39:52 +0800

    loss done

--- 00.框架/pytorch/class4/07.loss_bce.py
+++ 00.框架/pytorch/class4/07.loss_bce.py
@@ -1,125 +1,26 @@
-import numpy as np
 import torch
 import torch.nn as nn
 
-# fake data
-inputs = torch.tensor([[1, 2], [1, 3], [1, 3]], dtype=torch.float)
-target = torch.tensor([0, 1, 1], dtype=torch.long)
+# ----------------------------------- BCE Loss -----------------------------------
+inputs = torch.tensor([[1, 2], [2, 2], [3, 4], [4, 5]], dtype=torch.float)
+target = torch.tensor([[1, 0], [1, 0], [0, 1], [0, 1]], dtype=torch.float)
 
-# ----------------------------------- NLLLoss -----------------------------------
-# 这个loss就是对inputs中的每个样本中真实标签的那个值取反
-# flag = 0
-flag = 1
-if flag:
-    weights = torch.tensor([1, 1], dtype=torch.float)
+target_bce = target
 
-    loss_f_none_w = nn.NLLLoss(weight=weights, reduction='none')
-    loss_f_sum = nn.NLLLoss(weight=weights, reduction='sum')
-    loss_f_mean = nn.NLLLoss(weight=weights, reduction='mean')
+# itarget
+inputs = torch.sigmoid(inputs)
 
-    # forward
-    loss_none_w = loss_f_none_w(inputs, target)
-    loss_sum = loss_f_sum(inputs, target)
-    loss_mean = loss_f_mean(inputs, target)
+weights = torch.tensor([1, 1], dtype=torch.float)
 
-    print("NLL Loss: ")
-    print("\tweights: ", weights)
-    print("\tnone: {}\n\tsum: {}\n\tmean: {}".format(loss_none_w, loss_sum, loss_mean))
-# ----------------------------------- 3 BCE Loss -----------------------------------
-flag = 0
-# flag = 1
-if flag:
-    inputs = torch.tensor([[1, 2], [2, 2], [3, 4], [4, 5]], dtype=torch.float)
-    target = torch.tensor([[1, 0], [1, 0], [0, 1], [0, 1]], dtype=torch.float)
+loss_f_none_w = nn.BCELoss(weight=weights, reduction='none')
+loss_f_sum = nn.BCELoss(weight=weights, reduction='sum')
+loss_f_mean = nn.BCELoss(weight=weights, reduction='mean')
 
-    target_bce = target
+# forward
+loss_none_w = loss_f_none_w(inputs, target_bce)
+loss_sum = loss_f_sum(inputs, target_bce)
+loss_mean = loss_f_mean(inputs, target_bce)
 
-    # itarget
-    inputs = torch.sigmoid(inputs)
-
-    weights = torch.tensor([1, 1], dtype=torch.float)
-
-    loss_f_none_w = nn.BCELoss(weight=weights, reduction='none')
-    loss_f_sum = nn.BCELoss(weight=weights, reduction='sum')
-    loss_f_mean = nn.BCELoss(weight=weights, reduction='mean')
-
-    # forward
-    loss_none_w = loss_f_none_w(inputs, target_bce)
-    loss_sum = loss_f_sum(inputs, target_bce)
-    loss_mean = loss_f_mean(inputs, target_bce)
-
-    # view
-    print("\nweights: ", weights)
-    print("BCE Loss", loss_none_w, loss_sum, loss_mean)
-
-# --------------------------------- compute by hand
-flag = 0
-# flag = 1
-if flag:
-    idx = 0
-
-    x_i = inputs.detach().numpy()[idx, idx]
-    y_i = target.numpy()[idx, idx]  #
-
-    # loss
-    # l_i = -[ y_i * np.log(x_i) + (1-y_i) * np.log(1-y_i) ]      # np.log(0) = nan
-    l_i = -y_i * np.log(x_i) if y_i else -(1 - y_i) * np.log(1 - x_i)
-
-    # 输出loss
-    print("BCE inputs: ", inputs)
-    print("第一个loss为: ", l_i)
-
-# ----------------------------------- 4 BCE with Logis Loss -----------------------------------
-flag = 0
-# flag = 1
-if flag:
-    inputs = torch.tensor([[1, 2], [2, 2], [3, 4], [4, 5]], dtype=torch.float)
-    target = torch.tensor([[1, 0], [1, 0], [0, 1], [0, 1]], dtype=torch.float)
-
-    target_bce = target
-
-    # inputs = torch.sigmoid(inputs)
-
-    weights = torch.tensor([1, 1], dtype=torch.float)
-
-    loss_f_none_w = nn.BCEWithLogitsLoss(weight=weights, reduction='none')
-    loss_f_sum = nn.BCEWithLogitsLoss(weight=weights, reduction='sum')
-    loss_f_mean = nn.BCEWithLogitsLoss(weight=weights, reduction='mean')
-
-    # forward
-    loss_none_w = loss_f_none_w(inputs, target_bce)
-    loss_sum = loss_f_sum(inputs, target_bce)
-    loss_mean = loss_f_mean(inputs, target_bce)
-
-    # view
-    print("\nweights: ", weights)
-    print(loss_none_w, loss_sum, loss_mean)
-
-# --------------------------------- pos weight
-
-flag = 0
-# flag = 1
-if flag:
-    inputs = torch.tensor([[1, 2], [2, 2], [3, 4], [4, 5]], dtype=torch.float)
-    target = torch.tensor([[1, 0], [1, 0], [0, 1], [0, 1]], dtype=torch.float)
-
-    target_bce = target
-
-    # itarget
-    # inputs = torch.sigmoid(inputs)
-
-    weights = torch.tensor([1], dtype=torch.float)
-    pos_w = torch.tensor([3], dtype=torch.float)  # 3
-
-    loss_f_none_w = nn.BCEWithLogitsLoss(weight=weights, reduction='none', pos_weight=pos_w)
-    loss_f_sum = nn.BCEWithLogitsLoss(weight=weights, reduction='sum', pos_weight=pos_w)
-    loss_f_mean = nn.BCEWithLogitsLoss(weight=weights, reduction='mean', pos_weight=pos_w)
-
-    # forward
-    loss_none_w = loss_f_none_w(inputs, target_bce)
-    loss_sum = loss_f_sum(inputs, target_bce)
-    loss_mean = loss_f_mean(inputs, target_bce)
-
-    # view
-    print("\npos_weights: ", pos_w)
-    print(loss_none_w, loss_sum, loss_mean)
+# view
+print("\nweights: ", weights)
+print("BCE Loss", loss_none_w, loss_sum, loss_mean)
