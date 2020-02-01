8cbcda12a96cb64ce97aa3321fd990a2267b275d
sadicLiu <190978752@qq.com>
2020-02-01 10:58:26 +0800

    step lr

---
+++ 00.框架/pytorch/class5/06.lr_scheduler_lambda.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+LR = 0.1
+iteration = 10
+max_epoch = 200
+
+# %% fake data and optimizer
+
+weights = torch.randn((1), requires_grad=True)
+target = torch.zeros((1))
+
+optimizer = optim.SGD([weights], lr=LR, momentum=0.9)
+
+
+# ------------------------------ 5 Reduce LR On Plateau ------------------------------
+flag = 0
+# flag = 1
+if flag:
+    loss_value = 0.5
+    accuray = 0.9
+
+    factor = 0.1
+    mode = "min"
+    patience = 10
+    cooldown = 10
+    min_lr = 1e-4
+    verbose = True
+
+    scheduler_lr = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=factor, mode=mode, patience=patience,
+                                                        cooldown=cooldown, min_lr=min_lr, verbose=verbose)
+
+    for epoch in range(max_epoch):
+        for i in range(iteration):
+            # train(...)
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if epoch == 5:
+            loss_value = 0.4
+
+        scheduler_lr.step(loss_value)
+
+# ------------------------------ 6 lambda ------------------------------
+# flag = 0
+flag = 1
+if flag:
+
+    lr_init = 0.1
+
+    weights_1 = torch.randn((6, 3, 5, 5))
+    weights_2 = torch.ones((5, 5))
+
+    optimizer = optim.SGD([
+        {'params': [weights_1]},
+        {'params': [weights_2]}], lr=lr_init)
+
+    lambda1 = lambda epoch: 0.1 ** (epoch // 20)
+    lambda2 = lambda epoch: 0.95 ** epoch
+
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=[lambda1, lambda2])
+
+    lr_list, epoch_list = list(), list()
+    for epoch in range(max_epoch):
+        for i in range(iteration):
+            # train(...)
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+        scheduler.step()
+
+        lr_list.append(scheduler.get_lr())
+        epoch_list.append(epoch)
+
+        print('epoch:{:5d}, lr:{}'.format(epoch, scheduler.get_lr()))
+
+    plt.plot(epoch_list, [i[0] for i in lr_list], label="lambda 1")
+    plt.plot(epoch_list, [i[1] for i in lr_list], label="lambda 2")
+    plt.xlabel("Epoch")
+    plt.ylabel("Learning Rate")
+    plt.title("LambdaLR")
+    plt.legend()
+    plt.show()
